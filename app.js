e286712dc37cbf94806d5150dd8855abdc035fbf
Themis Papadopoulos <74245699+ThemPapadopoulos@users.noreply.github.com>
2021-04-14 21:52:50 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,11 @@
+
+      $('.imgslider').slick({
+            
+            dots: true,
+            infinite: true,
+            speed: 500,
+            fade: true,
+            cssEase: 'linear',
+            appendDots: $('.imgslider'),
+          });
+                                  
